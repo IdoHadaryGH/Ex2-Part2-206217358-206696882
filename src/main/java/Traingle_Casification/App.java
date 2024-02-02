aaccfc4dadbfefb054c5aba47a7494281966f962
Ido <ido.hadary1@gmail.com>
2024-02-02 16:10:43 +0200

    TC5_invalidData
    I expected to "ERROR" but it was "Nottriangle".

--- src/main/java/Traingle_Casification/App.java
+++ src/main/java/Traingle_Casification/App.java
@@ -10,19 +10,28 @@ class App {
 	private static Triangle_Types triangle;
 
 	public static void classifyTriangle(int a, int b, int c) {
-
-		if (isEquilateral(a, b, c)) {
+		
+		if (isInvalidData(a, b, c)) {
+			triangle = Triangle_Types.ERROR;
+		}
+		else if (isEquilateral(a, b, c)) {
 			triangle = Triangle_Types.equilateral;
-		} else if (isIsosceles(a, b, c)) {
+		} 
+		else if (isIsosceles(a, b, c)) {
 			triangle = Triangle_Types.isosceles;
-		} else if (!isTriangle(a, b, c)) {
+		} 
+		else if (!isTriangle(a, b, c)) {
 			triangle = Triangle_Types.Nottriangle;
-		} else {
+		} 
+		else {
 			triangle = Triangle_Types.Scalene;
 		}
 
 	}
-
+	private static boolean isInvalidData(int a, int b, int c) {
+		return !((a >= 0 && a <= 100) && (b >= 0 && b <= 100) && (c >= 0 && c <= 100));
+	}
+	
 	private static boolean isTriangle(int a, int b, int c) {
 		return a + b > c && a + c > b && b + c > a;
 	}
