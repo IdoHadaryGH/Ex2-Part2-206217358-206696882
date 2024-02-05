a95f50215c50cda4217257fbb85e04c794b10146
Ido <ido.hadary1@gmail.com>
2024-02-05 19:24:04 +0200

    Changes after the lecturer's notes
    Adding some test cases according to the lecturer's comments from the
    last lesson.

--- src/main/java/Traingle_Casification/App.java
+++ src/main/java/Traingle_Casification/App.java
@@ -11,10 +11,7 @@ class App {
 
 	public static void classifyTriangle(int a, int b, int c) {
 		
-		if (isInvalidData(a, b, c)) {
-			triangle = Triangle_Types.ERROR;
-		}
-		else if (isEquilateral(a, b, c)) {
+		if (isEquilateral(a, b, c)) {
 			triangle = Triangle_Types.equilateral;
 		} 
 		else if (isIsosceles(a, b, c)) {
@@ -28,9 +25,6 @@ class App {
 		}
 
 	}
-	private static boolean isInvalidData(int a, int b, int c) {
-		return !((a >= 0 && a <= 100) && (b >= 0 && b <= 100) && (c >= 0 && c <= 100));
-	}
 	
 	private static boolean isTriangle(int a, int b, int c) {
 		return a + b > c && a + c > b && b + c > a;
@@ -41,7 +35,7 @@ class App {
 	}
 
 	private static boolean isEquilateral(int a, int b, int c) {
-		return a + b > c && b + c > b && b + c > a && a == b && b == c;
+		return (a + b > c && a + c > b && b + c > a) && (a == b && b == c);
 	}
 
 	public static Triangle_Types get_type() {
--- src/test/java/Traingle_Casification/AppTest.java
+++ src/test/java/Traingle_Casification/AppTest.java
@@ -20,15 +20,35 @@ public class AppTest {
 	@Test
 	public void TC2_isoscelesTriangle() {
 		int a = 48;
-		int b = 34;
-		int c = 48;
+		int b = 48;
+		int c = 34;
 		triangle.classifyTriangle(a, b, c);
 		String expected = Triangle_Types.isosceles.toString();
 		String result = triangle.get_type().toString();
 		assertEquals(expected, result.toString());
 	}
 	@Test
-	public void TC3_scaleneTriangle() {
+	public void TC3_isoscelesTriangle() {
+		int a = 77;
+		int b = 25;
+		int c = 77;
+		triangle.classifyTriangle(a, b, c);
+		String expected = Triangle_Types.isosceles.toString();
+		String result = triangle.get_type().toString();
+		assertEquals(expected, result.toString());
+	}
+	@Test
+	public void TC4_isoscelesTriangle() {
+		int a = 8;
+		int b = 5;
+		int c = 5;
+		triangle.classifyTriangle(a, b, c);
+		String expected = Triangle_Types.isosceles.toString();
+		String result = triangle.get_type().toString();
+		assertEquals(expected, result.toString());
+	}
+	@Test
+	public void TC5_scaleneTriangle() {
 		int a = 44;
 		int b = 55;
 		int c = 66;
@@ -38,22 +58,42 @@ public class AppTest {
 		assertEquals(expected, result.toString());
 	}
 	@Test
-	public void TC4_notTriangle() {
+	public void TC6_notTriangle() {
 		int a = 10;
-		int b = 20;
-		int c = 33;
+		int b = 10;
+		int c = 30;
 		triangle.classifyTriangle(a, b, c);
 		String expected = Triangle_Types.Nottriangle.toString();
 		String result = triangle.get_type().toString();
 		assertEquals(expected, result.toString());
 	}
 	@Test
-	public void TC5_invalidData() {
-		int a = -15;
-		int b = 700;
-		int c = 26;
+	public void TC7_notTriangle() {
+		int a = 1;
+		int b = 7;
+		int c = 1;
 		triangle.classifyTriangle(a, b, c);
-		String expected = Triangle_Types.ERROR.toString();
+		String expected = Triangle_Types.Nottriangle.toString();
+		String result = triangle.get_type().toString();
+		assertEquals(expected, result.toString());
+	}
+	@Test
+	public void TC8_notTriangle() {
+		int a = 90;
+		int b = 10;
+		int c = 10;
+		triangle.classifyTriangle(a, b, c);
+		String expected = Triangle_Types.Nottriangle.toString();
+		String result = triangle.get_type().toString();
+		assertEquals(expected, result.toString());
+	}
+	@Test
+	public void TC9_notTriangle() {
+		int a = 1;
+		int b = 4;
+		int c = 6;
+		triangle.classifyTriangle(a, b, c);
+		String expected = Triangle_Types.Nottriangle.toString();
 		String result = triangle.get_type().toString();
 		assertEquals(expected, result.toString());
 	}
